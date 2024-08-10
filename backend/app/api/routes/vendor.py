9449c1cf68022987193f35a8d2c91fb5a1a644ce
Siddarth Tamse <sidtamse7@gmail.com>
2024-08-10 22:47:53 +0530

    create get_vendor,get_vendor_details,delete_vendor,update_existing_vendor

--- backend/app/api/routes/vendor.py
+++ backend/app/api/routes/vendor.py
@@ -1,11 +1,14 @@
+from typing import List
+
 from fastapi import APIRouter, HTTPException
 
 from fastapi import APIRouter, HTTPException
 from fastapi import status
 from fastapi.responses import JSONResponse
+from uuid import UUID
 
 from app.api.deps import SessionDep
-from app.crud import get_vendor_by_code, create_vendor
+from app.crud import get_vendor_by_code, create_vendor, get_vendors, delete_by_vendor, update_vendor
 from app.models import Vendor
 from app.schemas import vendor
 
@@ -30,4 +33,59 @@ def create_new_vendor(session: SessionDep, vendor_req: vendor.CreateVendor):
     return JSONResponse(
         status_code=status.HTTP_201_CREATED,
         content=vendor.to_vendor_res(new_vendor)
+    )
+
+
+@router.get("{vendor_code}", response_model=vendor.CreateVendorRes)
+def get_vendor(session: SessionDep, vendor_code: str):
+    existing_vendor = get_vendor_by_code(session=session, code=vendor_code)
+
+    if not existing_vendor:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Vendor status_code not found"
+        )
+    return vendor.to_vendor_res(existing_vendor)
+
+
+@router.get("/", response_model=List[vendor.CreateVendorRes])
+def get_vendor_details(session: SessionDep):
+    vendors = get_vendors(session=session)
+    return [vendor.to_vendor_res(v) for v in vendors]
+
+
+@router.delete("{vendor_code}")
+def delete_vendor(session: SessionDep, vendor_code: str):
+    vendor_to_delete = get_vendor_by_code(session=session, code=vendor_code)
+
+    if not vendor_to_delete:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Vendor code not found"
+        )
+
+    delete_by_vendor(session=session, vendor=vendor_to_delete)
+    return JSONResponse(
+        content="Vendor code successfully deleted"
+    )
+
+
+@router.put("/{vendor_id}", response_model=vendor.CreateVendorRes)
+def update_existing_vendor(session: SessionDep, vendor_id: UUID, vendor_req: vendor.CreateVendor):
+    updated_vendor = update_vendor(
+        session=session,
+        vendor_id=vendor_id,
+        vendor_code=vendor_req.vendor_code,
+        vendor_name=vendor_req.vendor_name
+    )
+
+    if updated_vendor is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Vendor not found"
+        )
+
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content=vendor.to_vendor_res(updated_vendor)
     )
