1c572a99dce1ccd73ba2ef5dd5cd87f18fe20421
Siddarth Tamse <sidtamse7@gmail.com>
2024-08-12 19:11:17 +0530

    added signin and vendor_form

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.routes import items, login, users, utils, vendor, student
+from app.api.routes import items, login, users, utils, vendor, student, signin, vendor_form
 
 
 api_router = APIRouter()
@@ -10,3 +10,5 @@ api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 api_router.include_router(items.router, prefix="/items", tags=["items"])
 api_router.include_router(vendor.router, prefix="/vendors", tags=["vendors"])
 api_router.include_router(student.router, prefix="/students", tags=["students"])
+api_router.include_router(signin.router, prefix="/signin", tags=["signin"])
+api_router.include_router(vendor_form.router, prefix="/vendor_form", tags=["vendor_form"])
