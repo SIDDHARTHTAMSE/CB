e6b7436cb0b834b9d9e37bb753786439cc0f349a
Siddarth Tamse <sidtamse7@gmail.com>
2024-08-16 13:36:32 +0530

    add contry, state, districrt, city, pincode, contact_no attributes in createVendor and CreateVendorRes class

--- backend/app/schemas/vendor.py
+++ backend/app/schemas/vendor.py
@@ -6,16 +6,34 @@ from app.models import Vendor
 class CreateVendor(BaseModel):
     vendor_code: str
     vendor_name: str
+    country: str
+    state: str
+    district: str
+    city: str
+    pincode: str
+    contact_no: str
 
 
 class CreateVendorRes(BaseModel):
     vendor_code: str
     vendor_name: str
+    country: str
+    state: str
+    district: str
+    city: str
+    pincode: str
+    contact_no: str
 
 
 def to_vendor_res(vendor: Vendor):
     """Convert DB model to response model"""
     return CreateVendorRes(
         vendor_code=vendor.vendor_code,
-        vendor_name=vendor.name
+        vendor_name=vendor.name,
+        country=vendor.country,
+        state=vendor.state,
+        district=vendor.district,
+        city=vendor.city,
+        pincode=vendor.pin_code,
+        contact_no=vendor.contact_number
     ).dict()
