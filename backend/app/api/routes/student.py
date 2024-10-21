0cfbb489dfffb4a8318983751e8d5799746e69fa
Siddarth Tamse <sidtamse7@gmail.com>
2024-10-21 12:17:54 +0530

    add update student api

--- backend/app/api/routes/student.py
+++ backend/app/api/routes/student.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, HTTPException, status
 from app.schemas.student import CreateStudent, CreateStudentRes
 from app.api.deps import SessionDep
 from app.crud import get_student_by_usn, create_student, get_student, delete_student, update_student
-from app.schemas.student import is_stud_res
+from app.schemas.student import is_stud_res, UpdateStudent
 from fastapi.responses import JSONResponse
 from app.models import Students
 from uuid import UUID
@@ -69,22 +69,22 @@ def delete_by_student_usn(session: SessionDep, student_usn: str):
 
 
 @router.put("{student_id}", response_model=CreateStudentRes)
-def update_by_student(session: SessionDep, student_id: UUID, student_req: CreateStudent):
-    existing_student = update_student(
-        session=session,
-        student_id=student_id,
-        student_first_name=student_req.student_first_name,
-        student_last_name=student_req.student_last_name,
-        student_usn=student_req.student_usn
-    )
-
-    if existing_student is None:
+def update_by_student(
+        session: SessionDep,
+        student_id: str,
+        student_req: UpdateStudent
+):
+    existing_student = get_student_by_usn(session=session, usn=student_id)
+    if not existing_student:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Student not found"
         )
+    existing_student.student_first_name = student_req.student_first_name or existing_student.student_first_name
+    existing_student.student_last_name = student_req.student_last_name or existing_student.student_last_name
 
+    existing_student = update_student(session=session, student=existing_student)
     return JSONResponse(
         status_code=status.HTTP_200_OK,
-        content="Updated successfully"
+        content=is_stud_res(existing_student)
     )
