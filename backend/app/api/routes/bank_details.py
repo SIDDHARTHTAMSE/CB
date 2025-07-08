7671a7fc9fe358ed3af24e129e3a2264cacc1d3a
SIDDHARTHTAMSE <sidtamse7@gmail.com>
2025-07-08 12:42:31 +0530

    add PUT api for bank user

--- backend/app/api/routes/bank_details.py
+++ backend/app/api/routes/bank_details.py
@@ -85,3 +85,26 @@ def get_bank_user_by_account_no(session: SessionDep, account_no: str):
             detail="User with account number not found"
         )
     return bank_details.to_bank_details_res(existing_account)
+
+
+@router.put("{account_no}", response_model=bank_details.CreateBankDetailsRes)
+def update_bank_user_by_account_no(
+        session: SessionDep,
+        account_no: str,
+        user_req: bank_details.UpdateBankDetails
+):
+    existing_account = get_bank_user_by_account(session=session, account_no=account_no)
+    if not existing_account:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User with account number not found"
+        )
+
+    existing_account.register_id = user_req.register_id or existing_account.register_id
+    existing_account.bank_name = user_req.bank_name or existing_account.bank_name
+    existing_account.account_holder_name = user_req.account_holder_name or existing_account.account_holder_name
+    existing_account.upi_id = user_req.upi_id or existing_account.upi_id
+    existing_account.ifsc_code = user_req.ifsc_code or existing_account.ifsc_code
+
+    update_user = update_bank_user(session=session, bank_user=existing_account)
+    return bank_details.to_bank_details_res(update_user)
