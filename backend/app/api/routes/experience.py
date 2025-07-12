3f0466d0d5af94fadf47680847e6bc8df51b779d
SIDDHARTHTAMSE <sidtamse7@gmail.com>
2025-07-12 20:35:08 +0530

    Add get by experience id and delete experience using id

--- backend/app/api/routes/experience.py
+++ backend/app/api/routes/experience.py
@@ -2,12 +2,13 @@ from typing import List
 from fastapi import APIRouter, HTTPException, status
 from fastapi.responses import JSONResponse
 from app.api.deps import SessionDep
+from uuid import UUID
 from app.crud import (
     create_experience,
-    get_experience_by_id,
+    get_experience_using_id,
     delete_experience,
     get_all_experience,
-    get_instructor_using_id,
+    get_instructor_by_id,
     update_experience
 )
 from app.models import Experience
@@ -18,9 +19,9 @@ router = APIRouter()
 
 @router.post("/", response_model=experience.CreateExperienceRes)
 def create_new_experience(session: SessionDep, user_req: experience.CreateExperience):
-    existing_instructor = get_instructor_using_id(
+    existing_instructor = get_instructor_by_id(
         session=session,
-        instructors_id=user_req.instructor_id
+        instructor_id=user_req.instructor_id
     )
 
     if not existing_instructor:
@@ -48,3 +49,35 @@ def get_all_experience_users(session: SessionDep):
     get_all_users = get_all_experience(session=session)
     return [experience.to_experience_res(s) for s in get_all_users]
 
+
+@router.get("{experience_id}", response_model=experience.CreateExperienceRes)
+def get_experience_by_id(session: SessionDep, experience_id: UUID):
+    existing_experience_user = get_experience_using_id(
+        session=session,
+        user_id=experience_id
+    )
+
+    if not existing_experience_user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Experience user not found"
+        )
+    return experience.to_experience_res(existing_experience_user)
+
+
+@router.delete("{experience_user_id}", response_model=experience.CreateExperienceRes)
+def delete_experience_user(session: SessionDep, experience_user_id: UUID):
+    existing_experience = get_experience_using_id(
+        session=session,
+        user_id=experience_user_id
+    )
+
+    if not existing_experience:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
+    delete_experience(session=session, experience=existing_experience)
+    return JSONResponse(
+        content="Experience user deleted successfully"
+    )
