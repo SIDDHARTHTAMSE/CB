6db7de2b39f4ab05acde4e0ba00faa83be3c9688
SIDDHARTHTAMSE <sidtamse7@gmail.com>
2025-08-05 22:19:43 +0530

    Add user device schema

---
+++ backend/app/schemas/user_device.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel, Field
+from uuid import UUID
+from typing import Optional
+from app.models import UserDevice
+from datetime import date
+
+
+class CreateUserDevice(BaseModel):
+    register_id: UUID
+    device_info: str
+    last_used: date
+    is_active: str
+    location: str
+
+
+class CreateUserDeviceRes(BaseModel):
+    pass
+
+
+class UpdateUserDevice(BaseModel):
+    register_id: Optional[UUID] = None
+    device_info: Optional[str] = None
+    last_used: Optional[date] = None
+    is_active: Optional[str] = None
+    location: Optional[str] = None
+
+
+def to_user_device_res(user_device: UserDevice):
+    return CreateUserDeviceRes(
+        register_id=user_device.register_id,
+        device_info=user_device.device_info,
+        last_used=user_device.last_used,
+        is_active=user_device.is_active,
+        location=user_device.location
+    )
