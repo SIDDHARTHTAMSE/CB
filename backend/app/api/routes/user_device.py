c785a9ce4c80aec083b35bd3e20ff2d477bb939a
SIDDHARTHTAMSE <sidtamse7@gmail.com>
2025-08-06 16:38:26 +0530

    Add Update user device using user device id

--- backend/app/api/routes/user_device.py
+++ backend/app/api/routes/user_device.py
@@ -51,3 +51,33 @@ def get_user_device_using_user_device(session: SessionDep, user_device_no: UUID)
             detail="User device is not found"
         )
     return user_device.to_user_device_res(existing_user_id)
+
+
+@router.put("{user_device_no}", response_model=user_device.CreateUserDeviceRes)
+def update_user_device_using_user_device_number(
+        session: SessionDep,
+        user_device_no: UUID,
+        user_req: user_device.UpdateUserDevice
+):
+    existing_register = get_register_id(session=session, register_id=user_req.register_id)
+    if not existing_register:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Register id is not found"
+        )
+
+    existing_user_device = get_user_device_using_id(session=session, user_id=user_device_no)
+    if not existing_user_device:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User device is not found"
+        )
+
+    existing_user_device.register_id = user_req.register_id or existing_register.register_id
+    existing_user_device.device_info = user_req.device_info or existing_user_device.device_info
+    existing_user_device.last_used = user_req.last_used or existing_user_device.last_used
+    existing_user_device.location = user_req.location or existing_user_device.location
+    existing_user_device.is_active = user_req.is_active or existing_user_device.is_active
+
+    updated_user_device = update_user_device(session=session, user_device=existing_user_device)
+    return user_device.to_user_device_res(updated_user_device)
