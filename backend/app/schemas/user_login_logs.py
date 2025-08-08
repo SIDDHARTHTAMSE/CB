ee23970e5923347c45b67db2896f1b771d19a09a
SIDDHARTHTAMSE <sidtamse7@gmail.com>
2025-08-08 21:17:59 +0530

    Add user login logs Schema

---
+++ backend/app/schemas/user_login_logs.py
@@ -0,0 +1,38 @@
+from pydantic import BaseModel
+from uuid import UUID
+from typing import Optional
+from app.models import UserLoginLogs
+from datetime import date
+
+
+class CreateUserLoginLogs(BaseModel):
+    register_id: UUID
+    log_id: str
+    login_time: date
+    logout_time: date
+    location: str
+    device: str
+
+
+class UserLogsLoginRes(CreateUserLoginLogs):
+    pass
+
+
+class UpdateUserLoginLogs(BaseModel):
+    register_id: Optional[UUID] = None
+    log_id: Optional[str] = None
+    login_time: Optional[date] = None
+    logout_time: Optional[date] = None
+    location: Optional[str] = None
+    device: Optional[str] = None
+
+
+def to_user_login_logs_res(user_logs: UserLogsLoginRes):
+    return UserLogsLoginRes(
+        register_id=user_logs.register_id,
+        log_id=user_logs.log_id,
+        login_time=user_logs.login_time,
+        logout_time=user_logs.logout_time,
+        location=user_logs.location,
+        device=user_logs.device
+    )
